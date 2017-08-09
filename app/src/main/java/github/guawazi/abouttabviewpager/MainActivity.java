0e92eada8fc79574b0c9051cdc3cb17ba64a6784
guawazi <guawazicoder@gmail.com>
2017-08-09 18:40:15 +0800

    创建项目

---
+++ app/src/main/java/github/guawazi/abouttabviewpager/MainActivity.java
@@ -0,0 +1,47 @@
+package github.guawazi.abouttabviewpager;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import github.guawazi.abouttabviewpager.adapter.HomePagerAdapter;
+import github.guawazi.abouttabviewpager.fragment.OneFragment;
+
+public class MainActivity extends AppCompatActivity implements OneFragment.OnFragmentInteractionListener {
+
+    private TabLayout mTabLayout;
+    private ViewPager mVpPager;
+    private OneFragment mOneFragment;
+    private OneFragment mTwoFragment;
+    private OneFragment mThreeFragment;
+    private HomePagerAdapter mHomePagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mVpPager = (ViewPager) findViewById(R.id.vp_pager);
+        mOneFragment = OneFragment.newInstance("第一个Fragment", "");
+        mTwoFragment = OneFragment.newInstance("第二个Fragment", "");
+        mThreeFragment = OneFragment.newInstance("第三个Fragment", "");
+    }
+
+    private void initData() {
+        mHomePagerAdapter = new HomePagerAdapter();
+        mVpPager.setAdapter(mHomePagerAdapter);
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/github/guawazi/abouttabviewpager/adapter/HomePagerAdapter.java
@@ -0,0 +1,20 @@
+package github.guawazi.abouttabviewpager.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/8/9 0009.
+ */
+
+public class HomePagerAdapter extends PagerAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
